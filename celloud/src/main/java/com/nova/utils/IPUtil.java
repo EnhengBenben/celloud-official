e266f1a924f28d767a64372b896e22aa349892ca
linyongchao <lycwork@126.com>
2015-07-14 11:37:16 +0800

    add:ip地址解析

--- celloud/src/main/java/com/nova/utils/IPUtil.java
+++ celloud/src/main/java/com/nova/utils/IPUtil.java
@@ -1,16 +1,28 @@
 package com.nova.utils;
 
-import com.handsome.ip.IPSeeker;
+import java.io.IOException;
+import java.nio.file.Paths;
+
+import com.github.jarod.qqwry.IPZone;
+import com.github.jarod.qqwry.QQWry;
 
 public class IPUtil {
 	/**
 	 * 根据ip地址获取所在地区
+	 * 
 	 * @param ip
 	 * @param fileDir
 	 * @return
 	 */
-	public static String getAddreeByIp(String ip,String fileDir){
-		IPSeeker ipSeeker = new IPSeeker("qqwry.dat", fileDir);
-		return ipSeeker.getIPLocation(ip).getCountry()+"-" + ipSeeker.getIPLocation(ip).getArea();
+	public static String getAddreeByIp(String ip, String fileDir) {
+		QQWry qqwry = null;
+		try {
+			qqwry = new QQWry(Paths.get(fileDir + "/qqwry.dat"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		String myIP = "223.223.192.105";
+		IPZone ipzone = qqwry.findIP(myIP);
+		return ipzone.getMainInfo() + "-" + ipzone.getSubInfo();
 	}
 }
