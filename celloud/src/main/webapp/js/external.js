1aa9fffe60765a67aaaca950ca89f8632b28c69f
sdyz5210 <sdyz5210@gmail.com>
2015-07-22 15:14:02 +0800

    删除zopim插件

--- celloud/src/main/webapp/js/external.js
+++ celloud/src/main/webapp/js/external.js
@@ -24,23 +24,3 @@ $(document).ready(function(){
 function closeVserionDiv(){
 	$(".alert").attr("style","display:none;");
 }
-
-/**
- * zopIm 聊天插件
- */
-window.$zopim || (function(d, s) {
-	var z = $zopim = function(c) {
-		z._.push(c)
-	}, $ = z.s = d.createElement(s), e = d.getElementsByTagName(s)[0];
-	z.set = function(o) {
-		z.set._.push(o)
-	};
-	z._ = [];
-	z.set._ = [];
-	$.async = !0;
-	$.setAttribute('charset', 'utf-8');
-	$.src = '//v2.zopim.com/?2Dh23fvDcnb1ijOwG7PsEZCcrp8sutR1';
-	z.t = +new Date;
-	$.type = 'text/javascript';
-	e.parentNode.insertBefore($, e)
-})(document, 'script');
