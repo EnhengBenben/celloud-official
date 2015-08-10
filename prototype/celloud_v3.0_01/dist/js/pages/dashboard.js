c771842893ea8eac626ae0d751e547de605f9262
daisly <leamoqinger_2011@163.com>
2015-08-10 15:15:20 +0800

    add:celloudv3.0原型设计第一版 #36

---
+++ prototype/celloud_v3.0_01/dist/js/pages/dashboard.js
@@ -0,0 +1,95 @@
+/*
+ * Author: Abdullah A Almsaeed
+ * Date: 4 Jan 2014
+ * Description:
+ *      This is a demo file used only for the main dashboard (index.html)
+ **/
+"use strict";
+
+$(function () {
+  //Make the dashboard widgets sortable Using jquery UI
+  $(".connectedSortable").sortable({
+    placeholder: "sort-highlight",
+    connectWith: ".connectedSortable",
+    handle: ".box-header, .nav-tabs",
+    forcePlaceholderSize: true,
+    zIndex: 999999
+  });
+  $(".connectedSortable .box-header, .connectedSortable .nav-tabs-custom").css("cursor", "move");
+
+  $('.daterange').daterangepicker(
+          {
+            ranges: {
+              'Today': [moment(), moment()],
+              'Yesterday': [moment().subtract('days', 1), moment().subtract('days', 1)],
+              'Last 7 Days': [moment().subtract('days', 6), moment()],
+              'Last 30 Days': [moment().subtract('days', 29), moment()],
+              'This Month': [moment().startOf('month'), moment().endOf('month')],
+              'Last Month': [moment().subtract('month', 1).startOf('month'), moment().subtract('month', 1).endOf('month')]
+            },
+            startDate: moment().subtract('days', 29),
+            endDate: moment()
+          },
+  function (start, end) {
+    alert("You chose: " + start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
+  });
+
+  //jvectormap data
+  var visitorsData = {
+    "US": 398, //USA
+    "SA": 400, //Saudi Arabia
+    "CA": 1000, //Canada
+    "DE": 500, //Germany
+    "FR": 760, //France
+    "CN": 300, //China
+    "AU": 700, //Australia
+    "BR": 600, //Brazil
+    "IN": 800, //India
+    "GB": 320, //Great Britain
+    "RU": 3000 //Russia
+  };
+ 
+  /* Morris.js Charts */
+  // Sales chart
+  var area = new Morris.Area({
+    element: 'revenue-chart',
+    resize: true,
+    data: [
+      {y: '2011 Q1', item1: 2666, item2: 2666},
+      {y: '2011 Q2', item1: 2778, item2: 2294},
+      {y: '2011 Q3', item1: 4912, item2: 1969},
+      {y: '2011 Q4', item1: 3767, item2: 3597},
+      {y: '2012 Q1', item1: 6810, item2: 1914},
+      {y: '2012 Q2', item1: 5670, item2: 4293},
+      {y: '2012 Q3', item1: 4820, item2: 3795},
+      {y: '2012 Q4', item1: 15073, item2: 5967},
+      {y: '2013 Q1', item1: 10687, item2: 4460},
+      {y: '2013 Q2', item1: 8432, item2: 5713}
+    ],
+    xkey: 'y',
+    ykeys: ['item1', 'item2'],
+    labels: ['Item 1', 'Item 2'],
+    lineColors: ['#a0d0e0', '#3c8dbc'],
+    hideHover: 'auto'
+  });
+  
+
+  //Donut Chart
+  var donut = new Morris.Donut({
+    element: 'sales-chart',
+    resize: true,
+    colors: ["#3c8dbc", "#f56954", "#00a65a"],
+    data: [
+      {label: "Download Sales", value: 12},
+      {label: "In-Store Sales", value: 30},
+      {label: "Mail-Order Sales", value: 20}
+    ],
+    hideHover: 'auto'
+  });
+
+  //Fix for charts under tabs
+  $('.box ul.nav a').on('shown.bs.tab', function (e) {
+    area.redraw();
+    donut.redraw();
+  });
+});
