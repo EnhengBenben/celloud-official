3146d502f2d25179f1b00fc86268d9f5e7ece9fd
daisly <leamoqinger_2011@163.com>
2015-11-06 10:21:41 +0800

    update:获取APP运行命令的方法 #180

--- celloud/src/main/java/com/celloud/action/DataAction.java
+++ celloud/src/main/java/com/celloud/action/DataAction.java
@@ -390,7 +390,7 @@ public class DataAction extends BaseAction {
                     task.setDataKey(datakey);
                     StringBuffer command = new StringBuffer(
                             "perl /share/biosoft/perl/PGS_MG/bin/moniter_qsub.pl perl ");
-                    command.append(perlMap.get(appId)).append(" ")
+                    command.append(appMap.get(appId).getCommand()).append(" ")
                             .append(dataListFile).append(" ").append(appPath)
                             .append(" ProjectID").append(proId);
                     task.setCommand(command.toString());
