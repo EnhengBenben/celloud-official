e15c09dd68adc55b00830412c6aadbee82f22ee8
linyongchao <lycwork@126.com>
2015-12-22 13:58:06 +0800

    fixed:修复了报告pdf无法下载的bug

--- celloud/src/main/webapp/js/report.js
+++ celloud/src/main/webapp/js/report.js
@@ -28,7 +28,7 @@ $.ajaxSetup ({
 			$.get("project!downPdf",{"userId":userId,"softwareId":softwareId,"projectId":projectId},function(userNames){
 				userNames = userNames.replace(/"/g ,"").replace(/\\/g ,"");
 				if(userNames){
-					var url = window.location.href.split("celloud")[0];
+					var url = window.location.href.split("index")[0];
 					window.location.href=url+"Tools/Procedure!miRNADownload?userId="+userNames;
 				}else{
 					jAlert("没有可以下载的pdf文件");
