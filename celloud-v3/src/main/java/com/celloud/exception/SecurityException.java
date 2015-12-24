568eb2d3b8f2b108ca2ffb2e10044de63862c460
window sun <sun8wd@gmail.com>
2015-12-24 10:50:12 +0800

    完成feedback功能

--- celloud-v3/src/main/java/com/celloud/exception/SecurityException.java
+++ celloud-v3/src/main/java/com/celloud/exception/SecurityException.java
@@ -1,20 +1,20 @@
 package com.celloud.exception;
 
 /**
- * 业务异常
+ * 安全异常，主要用来校验用户是否已登录
  * 
  * @author <a href="sunwendong@celloud.cn">sun8wd</a>
- * @date 2015年12月23日 下午4:11:19
+ * @date 2015年12月23日 下午4:11:54
  */
-public class BusinessException extends RuntimeException {
+public class SecurityException extends RuntimeException{
     private static final long serialVersionUID = 1L;
-    private String msg;
+    private String msg="当前用户未登录或登录已超时！";
 
-    public BusinessException() {
+    public SecurityException() {
         super();
     }
 
-    public BusinessException(String msg) {
+    public SecurityException(String msg) {
         super(msg);
         this.msg = msg;
     }
