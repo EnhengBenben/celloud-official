83ad35d389c80a3a29dbf55c706c64864d34fff1
linyongchao <lycwork@126.com>
2016-01-07 09:51:40 +0800

    update:修改了action中获取userId的方法

--- celloud-v3/src/main/java/com/celloud/action/ReportAction.java
+++ celloud-v3/src/main/java/com/celloud/action/ReportAction.java
@@ -2,10 +2,8 @@ package com.celloud.action;
 
 import java.util.Date;
 import java.util.Map;
-import java.util.Map.Entry;
 
 import javax.annotation.Resource;
-import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.celloud.constants.Constants;
-import com.celloud.model.User;
+import com.celloud.constants.ConstantsData;
 import com.celloud.page.Page;
 import com.celloud.page.PageList;
 import com.celloud.service.ReportService;
@@ -26,33 +24,19 @@ public class ReportAction {
 
     @RequestMapping("getReportPageList")
     public ModelAndView reportPages(
-            HttpSession session,
             @RequestParam(defaultValue = "1") Integer page,
             @RequestParam(defaultValue = Constants.DEFAULT_PAGE_SIZE + "") Integer size,
             String condition, String start, String end, Integer appId) {
         long s = new Date().getTime();
-        System.out.println("------"+page);
-        System.out.println(size);
         System.out.println(condition);
         System.out.println(start);
         System.out.println(end);
         System.out.println(appId);
-//        Constants.SESSION_LOGIN_USER
-        User user = (User) session.getAttribute(Constants.SESSION_LOGIN_USER);
-        System.out.println(user==null);
-        System.out.println(user.getUserId());
+        Integer userId = ConstantsData.getLoginUserId();
         ModelAndView mv = new ModelAndView("report/report_list");
         Page pager = new Page(page, size);
         PageList<Map<String, Object>> pageList = reportService
-                .getReportPageList(user.getUserId(), pager, condition, start, end, appId);
-        System.out.println(pageList.getDatas().size());
-        // System.out.println(pageList.getDatas().get(0).get("project_id"));
-        Map<String, Object> map = pageList.getDatas().get(0);
-        for (Entry<String, Object> i : map.entrySet()) {
-            System.out.println("key:" + i.getKey());
-            System.out.println("val:" + i.getValue());
-            System.out.println("--");
-        }
+                .getReportPageList(userId, pager, condition, start, end, appId);
         long e = new Date().getTime();
         System.out.println(e - s);
         return mv.addObject("pageList", pageList);
