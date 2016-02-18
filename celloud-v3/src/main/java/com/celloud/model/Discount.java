6727de189046527d1615b7f738851e8560cba3ab
linyongchao <lycwork@126.com>
2016-02-18 18:15:08 +0800

    add:新增tb_price和tb_discount的model和dao

---
+++ celloud-v3/src/main/java/com/celloud/model/Discount.java
@@ -0,0 +1,76 @@
+package com.celloud.model;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class Discount {
+    private Integer id;
+
+    private Integer priceId;
+
+    private Float discountRate;
+
+    private BigDecimal discountPrice;
+
+    private Date startDate;
+
+    private Date endDate;
+
+    private Date createDate;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getPriceId() {
+        return priceId;
+    }
+
+    public void setPriceId(Integer priceId) {
+        this.priceId = priceId;
+    }
+
+    public Float getDiscountRate() {
+        return discountRate;
+    }
+
+    public void setDiscountRate(Float discountRate) {
+        this.discountRate = discountRate;
+    }
+
+    public BigDecimal getDiscountPrice() {
+        return discountPrice;
+    }
+
+    public void setDiscountPrice(BigDecimal discountPrice) {
+        this.discountPrice = discountPrice;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+}
