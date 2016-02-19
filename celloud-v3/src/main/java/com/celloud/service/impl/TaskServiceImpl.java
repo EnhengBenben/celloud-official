058ad48ef369274d53b423f6d445370ae04bc82a
daisly <leamoqinger_2011@163.com>
2016-02-19 16:11:27 +0800

    update:MIB运行完成一个报告修改一次项目简报

--- celloud-v3/src/main/java/com/celloud/service/impl/TaskServiceImpl.java
+++ celloud-v3/src/main/java/com/celloud/service/impl/TaskServiceImpl.java
@@ -78,13 +78,16 @@ public class TaskServiceImpl implements TaskService {
 
         int runNum = reportMapper.selectRunNumByPro(projectId,
                 DataState.ACTIVE, ReportType.DATA, ReportPeriod.COMPLETE);
+        report.setFlag(ReportType.PROJECT);
+        report.setFileId(null);
+        report.setContext(context);
         if (runNum == 0) {
-            report.setFlag(ReportType.PROJECT);
+            report.setPeriod(ReportPeriod.COMPLETE);
             report.setEndDate(new Date());
-            report.setContext(context);
-            report.setFileId(null);
-            reportMapper.updateReportPeriod(report);
+        } else {
+            report.setPeriod(ReportPeriod.RUNNING_HAVE_REPORT);
         }
+        reportMapper.updateReportPeriod(report);
         return task;
     }
 
