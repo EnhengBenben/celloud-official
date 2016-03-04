8869cbe15b8938b908e360f39e6f322c93fb200a
linyongchao <lycwork@126.com>
2016-03-04 14:46:11 +0800

    delete:删除了旧的celloud项目

--- celloud/src/main/java/com/nova/constants/SoftWareOffLineState.java
+++
@@ -1,6 +0,0 @@
-package com.nova.constants;
-
-public class SoftWareOffLineState {
-    public static final int ON = 0;
-    public static final int OFF = 1;
-}
--- celloud/src/main/java/com/nova/dao/impl/BaseDao.java
+++
@@ -1,7 +0,0 @@
-package com.nova.dao.impl;
-
-import com.opensymphony.xwork2.ActionContext;
-
-public class BaseDao {
-	protected String userName = String.valueOf(ActionContext.getContext().getSession().get("userName"));
-}
