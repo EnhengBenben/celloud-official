6dc1a4f5fe0f1233181b7688932186bbbf0f2fff
daisly <leamoqinger_2011@163.com>
2016-03-10 10:31:04 +0800

    fixed:数据管理运行时选择已运行的数据再取消选择时的bug

--- celloud/src/main/webapp/resources/js/data.js
+++ celloud/src/main/webapp/resources/js/data.js
@@ -562,7 +562,7 @@ function _init_data(){
     },
     noCheck: function(dataId){
       $("#data-checkall").prop("checked",false);
-      var index = $.inArray(dataId, $.dataManager.options.checkedIds);
+      var index = $.inArray(dataId, $.dataManager.options.checkedIds) + 1;
       $.dataManager.options.checkedIds.splice(index, 1);
       $.dataManager.options.checkedNames.splice(index, 1);
     }
