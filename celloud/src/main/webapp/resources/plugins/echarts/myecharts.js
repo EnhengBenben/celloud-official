cf66ac6404188f65f80b566d8f45aa6549b0de3d
miaoqi <2127983862@qq.com>
2016-04-12 17:11:41 +0800

    update:tbinh饼图修改

--- celloud/src/main/webapp/resources/plugins/echarts/myecharts.js
+++ celloud/src/main/webapp/resources/plugins/echarts/myecharts.js
@@ -1,32 +1,29 @@
-function echartsShowPie(id, title, data) {
-	
-	var echartsPie = echarts.init(document.getElementById(id));
-	
-	var option = {
-		title : {
-	        text: title,
-	        x:'center'
-	    },
-	    tooltip : {
-	        trigger: 'item',
-	        formatter: "{a} <br/>{b} : {c} ({d}%)"
-	    },
-	    series : [
-	        {
-	            type: 'pie',
-	            radius : '55%',
-	            center: ['50%', '60%'],
-	            data:data,
-	            itemStyle: {
-	                emphasis: {
-	                    shadowBlur: 10,
-	                    shadowOffsetX: 0,
-	                    shadowColor: 'rgba(0, 0, 0, 0.5)'
-	                }
-	            }
-	        }
-	    ]
-	}
-	
-	echartsPie.setOption(option);
-}
+function showPie(id,title,data) {
+    	var echartsPie = echarts.init(document.getElementById(id));
+        var option = {
+            title : {
+                text: title,
+                x:'center'
+            },
+            tooltip : {
+                trigger: 'item',
+                formatter: "{a} <br/>{b} : {c} ({d}%)"
+            },
+            series : [
+                {
+                    type: 'pie',
+                    radius : '55%',
+                    center: ['50%', '60%'],
+                    data:data,
+                    itemStyle: {
+                        emphasis: {
+                            shadowBlur: 10,
+                            shadowOffsetX: 0,
+                            shadowColor: 'rgba(0, 0, 0, 0.5)'
+                        }
+                    }
+                }
+            ]
+        }
+        echartsPie.setOption(option);
+    }
