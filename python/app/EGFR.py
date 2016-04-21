0edef8852ad75997a1824f6e9a49a3eafa47e7c2
miaomiaoqi <2127983862@qq.com>
2016-04-21 14:07:03 +0800

    update:egfr.py解决冲突

--- python/app/EGFR.py
+++ python/app/EGFR.py
@@ -64,23 +64,6 @@ class EGFR:
 			#截取数据报告路径
 			paths = path.split(os.sep);
 			#首先判断用户id是否为测试用户
-<<<<<<< HEAD
-			if(paths[len(paths) - 3] not in PDFPro.userList):
-				#判断mongo中是否有对应dataKey的数据, 如果有则都删除
-				#获取mongo操作类实例
-				mo = mongo.getInstance();
-				if(mo.findAllByCondition({'dataKey':paths[len(paths)-1]},'EGFRCount').count() > 0):
-					mo.deleteAllByCondition({'dataKey':paths[len(paths)-1]},'EGFRCount');
-				#打开报告
-				f = codecs.open(report,'r','gbk');
-				#位点的dict
-				resultCount = {};
-				#读取第一行将\n替换,并将空格替换为\t方便统一操作
-				firstLine = f.readline().replace('\n','').replace(' ','\t').strip();
-				if((firstLine.startswith('Exon') and firstLine.split('\t') == 2) or (firstLine.startswith('EGFR') and firstLine.split('\t') == 5)):
-					#使用\t分割
-					firstLines = firstLine.split('\t');
-=======
 			if(paths[len(paths) - 4] not in PDFPro.userList):
 				#判断mongo中是否有对应dataKey的数据, 如果有则都删除
 				#获取mongo操作类实例
@@ -91,27 +74,17 @@ class EGFR:
 				resultCount = {};
 				
 				if((firstLine.startswith('Exon') and len(firstLines) == 2) or (firstLine.startswith('EGFR') and len(firstLines) == 5)):
->>>>>>> release_3.1.7
 					#截取userId
 					resultCount['userId'] = int(paths[len(paths)-4]);
 					#截取dataKey
 					resultCount['dataKey'] = paths[len(paths)-2];
 					#截取length
 					resultCount['length'] = int(firstLines[len(firstLines) - 1]);
-<<<<<<< HEAD
-					result['pos'] = int(firstLines[len(firstLines) - 1]);
-=======
->>>>>>> release_3.1.7
 					list = [];
 					#循环读取剩余的行
 					while True:
 						line = f.readline().strip();
-<<<<<<< HEAD
-						print(line);
-						if line:
-=======
 						if line and line.startswith('SNP'):
->>>>>>> release_3.1.7
 							resultCount['site'] = 0;
 							lines = line.split('\t');
 							target = lines[len(lines) - 2]
@@ -134,13 +107,6 @@ class EGFR:
 								resultCount['site'] = int(lines[1]);
 							if('site' in resultCount.keys() and resultCount['site'] != 0):
 								list.append(resultCount.copy());
-<<<<<<< HEAD
-						else:
-							break;
-					# 执行批量插入操作
-					mo.insertBatch(list,'EGFRCount');
-				f.close();
-=======
 						elif line and line.startswith('Indel'):
 							lines = line.split('\t');
 							resultCount['site'] = int(lines[3]);
@@ -151,7 +117,6 @@ class EGFR:
 					if(len(list) > 0):
 						mo.insertBatch(list,'EGFRCount');
 			f.close();
->>>>>>> release_3.1.7
 
 		#report.txt.wz.1
 		wz1 = os.path.join(path,'report.txt.wz.1')
