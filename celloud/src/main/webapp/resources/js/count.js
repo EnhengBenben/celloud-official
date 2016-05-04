a8eee22a1c2a30bb97716c5abdd6f47bdb3d4154
linyongchao <lycwork@126.com>
2016-05-04 10:59:15 +0800

    add:统计页面增加loading效果 #538

--- celloud/src/main/webapp/resources/js/count.js
+++ celloud/src/main/webapp/resources/js/count.js
@@ -1,9 +1,31 @@
 $(document).ready(function() {
+  var opts = {
+      lines: 13, // The number of lines to draw
+      length: 28, // The length of each line
+      width: 14, // The line thickness
+      radius: 42, // The radius of the inner circle
+      corners: 1, // Corner roundness (0..1)
+      rotate: 0, // The rotation offset
+      direction: 1, // 1: clockwise, -1: counterclockwise
+      color: '#000', // #rgb or #rrggbb or array of colors
+      speed: 1, // Rounds per second
+      trail: 60, // Afterglow percentage
+      shadow: false, // Whether to render a shadow
+      hwaccel: false, // Whether to use hardware acceleration
+      className: 'spinner', // The CSS class to assign to the spinner
+      zIndex: 2e9, // The z-index (defaults to 2000000000)
+      top: '40%', // Top position relative to parent in px
+      left: '45%' // Left position relative to parent in px
+    };
 	//showSysInfo();
+  spinner = new Spinner(opts);
+  var target = document.getElementById('appMain');
+  spinner.spin(target);
 	var companyId = $("#sessionCompanyId").val();
 	if (companyId == 6) {
 		$.get("count/pgsCount",function(responseText){
 			$("#countDiv").html(responseText);
+			spinner.stop();
 		});
 	} else if (companyId == 3) {
 		$.get("count/hbvCount",function(responseText){
@@ -13,12 +35,14 @@ $(document).ready(function() {
 			  $("#_down").attr("href", url);
 			  $("#_down").removeClass("hide");
 			}
+			spinner.stop();
 		});
 	} else if (companyId == 33) {
 		$.get("count/cmpCount",function(responseText){
 			$("#countDiv").html(responseText);
 			var url = $("#downUrl").val();
 			$("#_down").attr("href", url);
+			spinner.stop();
 		});
 	}
 });
