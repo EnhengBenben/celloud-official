3f59ac25d9e3edd170b1c059ba0aa5ccafbaa2b2
miaomiaoqi <2127983862@qq.com>
2016-05-19 19:53:22 +0800

    update:egfr使用velocity模板

--- celloud/src/main/java/com/celloud/action/ReportAction.java
+++ celloud/src/main/java/com/celloud/action/ReportAction.java
@@ -1146,17 +1146,22 @@ public class ReportAction {
     @ActionLog(value = "打印EGFR数据报告", button = "打印数据报告")
     @RequestMapping("printEGFR")
     public ModelAndView printEGFR(Integer appId, String dataKey, Integer projectId) {
-        ModelAndView mv = getModelAndView("print/print_egfr", projectId);
+        String path = ConstantsData.getLoginCompanyId() + "/" + appId + "/print.vm";
+        if (ReportAction.class.getResource("/templates/report/" + path) == null) {
+            path = "default/" + appId + "/print.vm";
+        }
+        ModelAndView mv = new ModelAndView(path);
+        EGFR egfr = reportService.getEGFRReport(dataKey, projectId, appId);
         Integer userId = ConstantsData.getLoginUserId();
         Integer fileId = dataService.getDataByKey(dataKey).getFileId();
         Report report = reportService.getReport(userId, appId, projectId, fileId, ReportType.DATA);
         // 首先检索该报告是否保存过，若保存过，则直接将保存内容返回
         if (StringUtils.isNotEmpty(report.getPrintContext())) {
-            return mv.addObject("printContext", report.getPrintContext());
+            mv.addObject("printContext", report.getPrintContext());
         }
-        EGFR egfr = reportService.getEGFRReport(dataKey, projectId, appId);
         mv.addObject("egfr", egfr).addObject("report", report);
         return mv;
+
     }
 
     /**
