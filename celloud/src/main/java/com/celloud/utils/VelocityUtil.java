49f8d0445996ae374311bdf9779c83524542b25b
window sun <sun8wd@gmail.com>
2016-08-04 18:55:20 +0800

    update:修改velocity模板传入的base参数

--- celloud/src/main/java/com/celloud/utils/VelocityUtil.java
+++ celloud/src/main/java/com/celloud/utils/VelocityUtil.java
@@ -16,47 +16,47 @@ import org.apache.velocity.tools.generic.NumberTool;
 import com.celloud.constants.ConstantsData;
 
 public class VelocityUtil {
-    @Resource
-    private VelocityEngine velocityEngine;
-    private static final String MAIL_TEMPLATE_PATH = "templates/mail";
-    private static final String REPORT_TEMPLATE_PATH = "templates/report";
+	@Resource
+	private VelocityEngine velocityEngine;
+	private static final String MAIL_TEMPLATE_PATH = "templates/mail";
+	private static final String REPORT_TEMPLATE_PATH = "templates/report";
 
-    public String mergeMailTemplate(String template, Map<String, Object> context) {
-        if (!template.startsWith("/")) {
-            template = "/" + template;
-        }
-        template = MAIL_TEMPLATE_PATH + template;
-        return mergeTemplate(template, context);
-    }
+	public String mergeMailTemplate(String template, Map<String, Object> context) {
+		if (!template.startsWith("/")) {
+			template = "/" + template;
+		}
+		template = MAIL_TEMPLATE_PATH + template;
+		return mergeTemplate(template, context);
+	}
 
-    public String mergeReportTemplate(String template, Map<String, Object> context) {
-        if (!template.startsWith("/")) {
-            template = "/" + template;
-        }
-        template = REPORT_TEMPLATE_PATH + template;
-        return mergeTemplate(template, context);
-    }
+	public String mergeReportTemplate(String template, Map<String, Object> context) {
+		if (!template.startsWith("/")) {
+			template = "/" + template;
+		}
+		template = REPORT_TEMPLATE_PATH + template;
+		return mergeTemplate(template, context);
+	}
 
-    private String mergeTemplate(String template, Map<String, Object> context) {
-        if (context == null) {
-            context = new HashMap<>();
-        }
-        Template temp = velocityEngine.getTemplate(template, "utf-8");
-        StringWriter writer = new StringWriter();
-        context.put("dateTool", new DateTool());
-        context.put("numberTool", new NumberTool());
-        context.put("base", ConstantsData.getContextUrl());
-        context.put("rightDate", new Date());
-        temp.merge(new VelocityContext(context), writer);
-        return writer.toString();
-    }
+	private String mergeTemplate(String template, Map<String, Object> context) {
+		if (context == null) {
+			context = new HashMap<>();
+		}
+		Template temp = velocityEngine.getTemplate(template, "utf-8");
+		StringWriter writer = new StringWriter();
+		context.put("dateTool", new DateTool());
+		context.put("numberTool", new NumberTool());
+		context.put("base", ConstantsData.getRequset().getContextPath());
+		context.put("rightDate", new Date());
+		temp.merge(new VelocityContext(context), writer);
+		return writer.toString();
+	}
 
-    public VelocityEngine getVelocityEngine() {
-        return velocityEngine;
-    }
+	public VelocityEngine getVelocityEngine() {
+		return velocityEngine;
+	}
 
-    public void setVelocityEngine(VelocityEngine velocityEngine) {
-        this.velocityEngine = velocityEngine;
-    }
-    
+	public void setVelocityEngine(VelocityEngine velocityEngine) {
+		this.velocityEngine = velocityEngine;
+	}
+
 }
