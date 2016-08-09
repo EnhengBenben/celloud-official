73380e0464595d320c385aa04f377823ea7dd3cc
miaomiaoqi <2127983862@qq.com>
2016-08-09 10:54:48 +0800

    update:更新manager周统计

---
+++ manager/src/main/java/com/celloud/manager/service/WeekService.java
@@ -0,0 +1,21 @@
+package com.celloud.manager.service;
+
+import com.celloud.manager.model.Week;
+
+/**
+ * @author MQ:
+ * @date 2016年8月8日 下午6:09:00
+ * @description 周统计接口
+ */
+public interface WeekService {
+    /**
+     * 
+     * @author MQ
+     * @date 2016年8月8日下午6:09:18
+     * @description 添加一条周记录
+     * @param week
+     *            周统计对象
+     *
+     */
+    public void add(Week week);
+}
