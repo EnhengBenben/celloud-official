b836313ace32d9f7457fe987450d95b683f4c637
linyongchao <lycwork@126.com>
2016-09-08 10:11:34 +0800

    fixed:修复了从其他页面切换到工作台时滚动条不置顶的bug #1041

--- celloud/src/main/webapp/resources/js/overview/controller.js
+++ celloud/src/main/webapp/resources/js/overview/controller.js
@@ -4,6 +4,7 @@
     var height = width/2.5;
     $(".count-box-body").width(width);
     $(".count-box-body").height(height);
+    $("body").scrollTop(0);
     loginCount.loginCount().success(function(response){
       $scope.map = response;
       userCount.fileDayCount();
