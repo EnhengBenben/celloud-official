59e985efdb4450dcea37291b48af31037f1671e6
linyongchao <lycwork@126.com>
2016-09-12 01:14:18 +0800

    update:修改了华木兰相关的标签sql

--- dbUpgrade/3.3.0/tag.sql
+++ dbUpgrade/3.3.0/tag.sql
@@ -9,7 +9,6 @@ insert into tb_tag (tag_name) select app_name from tb_app where off_line =  0 an
 insert into tb_tag (tag_name) select app_name from tb_app where off_line =  0 and company_id=0 and run_type!=0;
 
 insert into tb_app_tag_relat (app_id,tag_id) values (118,1);
-insert into tb_app_tag_relat (app_id,tag_id) values (123,2);
 insert into tb_app_tag_relat (app_id,tag_id) select app_id,tag_id from tb_app a,tb_tag t where a.app_name=t.tag_name;
 
 insert into tb_file_tag_relat (file_id,tag_id) select r.file_id,t.tag_id from (select file_id,app_id from (select a.file_id,a.app_id,count(app_id) as app from (select file_id,app_id,count(*) from tb_report group by file_id,app_id) as a group by a.file_id ) b where b.app=1) r,tb_app_tag_relat t where r.app_id=t.app_id;
