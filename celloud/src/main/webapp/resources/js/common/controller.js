fec68ea26cd3b0eb01ddc63ff9dd7d57a43252d8
window sun <sun8wd@gmail.com>
2016-09-11 23:41:47 +0800

    update:尝试修复桌面提醒出现多次的问题

--- celloud/src/main/webapp/resources/js/common/controller.js
+++ celloud/src/main/webapp/resources/js/common/controller.js
@@ -85,30 +85,6 @@
 			$("#tips-modal").modal("show");
 		}
 		
-		/**
-		 * 监听userMessage频道，有新消息时，刷新右上角提醒
-		 */
-		messageUtils.subscribe("userMessage", function(data) {
-			$rootScope.messages = commonService.messages.get();
-			messageUtils.notify(data.noticeTitle, data.noticeContext, {}, {
-				"onclick" : function() {
-					var notification = this;
-					notification.close();
-				}
-			});
-		});
-		/**
-		 * 监听userNotice频道，有新消息时，刷新右上角提醒
-		 */
-		messageUtils.subscribe("userNotice", function(data) {
-			$rootScope.notices = commonService.notices.get();
-			messageUtils.notify(data.noticeTitle, data.noticeContext, {}, {
-				"onclick" : function() {
-					var notification = this;
-					notification.close();
-				}
-			});
-		});
 		
 		/**
 		 * 修改更多按钮
@@ -130,3 +106,27 @@
 		}
 	});
 })();
+/**
+ * 监听userMessage频道，有新消息时，刷新右上角提醒
+ */
+messageUtils.subscribe("userMessage", function(data) {
+	$rootScope.messages = commonService.messages.get();
+	messageUtils.notify(data.noticeTitle, data.noticeContext, {}, {
+		"onclick" : function() {
+			var notification = this;
+			notification.close();
+		}
+	});
+});
+/**
+ * 监听userNotice频道，有新消息时，刷新右上角提醒
+ */
+messageUtils.subscribe("userNotice", function(data) {
+	$rootScope.notices = commonService.notices.get();
+	messageUtils.notify(data.noticeTitle, data.noticeContext, {}, {
+		"onclick" : function() {
+			var notification = this;
+			notification.close();
+		}
+	});
+});
