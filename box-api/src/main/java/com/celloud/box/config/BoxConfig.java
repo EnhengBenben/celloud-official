054311dd925caef260269139dc3e629a65ef9db9
window sun <sun8wd@gmail.com>
2016-09-19 14:29:02 +0800

    update:添加不同环境的配置文件

--- box-api/src/main/java/com/celloud/box/config/BoxConfig.java
+++ box-api/src/main/java/com/celloud/box/config/BoxConfig.java
@@ -10,6 +10,21 @@ public class BoxConfig {
 	 * 文件上传路径
 	 */
 	private String uploadPath;
+	/**
+	 * 当前的运行环境：<br />
+	 * dev=开发环境
+	 * test=测试环境
+	 * prod=生产环境
+	 */
+	private String env;
+
+	public String getEnv() {
+		return env;
+	}
+
+	public void setEnv(String env) {
+		this.env = env;
+	}
 
 	public String getUploadPath() {
 		return uploadPath;
