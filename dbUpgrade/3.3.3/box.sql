397ba1dfdbc0c09aa3befd2da0e47257218577cf
window sun <sun8wd@gmail.com>
2016-09-22 16:59:49 +0800

    update:修改盒子相关的数据库脚本错误

--- dbUpgrade/3.3.3/box.sql
+++ dbUpgrade/3.3.3/box.sql
@@ -1,3 +1,3 @@
-ALTER TABLE `celloud-box`.`tb_file` 
+ALTER TABLE `celloud`.`tb_file` 
 ADD COLUMN `oss_path` VARCHAR(225) NULL COMMENT '文件在oss上的路径，即oss的objectKey' AFTER `batch`,
 ADD COLUMN `upload_state` INT(11) NOT NULL DEFAULT 0 COMMENT '文件上传状态:0=已上传到盒子，1=已上传到oss，2=上传oss失败' AFTER `oss_path`;
