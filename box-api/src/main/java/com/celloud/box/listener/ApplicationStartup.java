36ef73a3debf53ad8aa9c2ef8d662eb08919c480
window sun <sun8wd@gmail.com>
2016-09-22 13:49:02 +0800

    update:springboot启动时输出一些配置属性

--- box-api/src/main/java/com/celloud/box/listener/ApplicationStartup.java
+++ box-api/src/main/java/com/celloud/box/listener/ApplicationStartup.java
@@ -2,6 +2,8 @@ package com.celloud.box.listener;
 
 import javax.annotation.Resource;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -10,12 +12,19 @@ import com.celloud.box.utils.UploadPath;
 
 @Component
 public class ApplicationStartup implements CommandLineRunner {
+	private static Logger logger = LoggerFactory.getLogger(ApplicationStartup.class);
 	@Resource
 	private BoxConfig config;
 
 	@Override
 	public void run(String... arg0) throws Exception {
 		UploadPath.setRootPath(config.getUploadPath());
+		logger.info("********************* System Properties *********************");
+		logger.info("");
+		logger.info("box.env        =  {}", config.getEnv());
+		logger.info("box.uploadPath =  {}", config.getUploadPath());
+		logger.info("");
+		logger.info("*************************************************************");
 	}
 
 }
