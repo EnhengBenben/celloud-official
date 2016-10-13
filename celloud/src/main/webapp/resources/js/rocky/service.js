a06425a0b57b4113953f65c97158aea763246df2
daisly <leamoqinger_2011@163.com>
2016-10-13 15:05:40 +0800

    delete:重复的angular service

--- celloud/src/main/webapp/resources/js/rocky/service.js
+++ celloud/src/main/webapp/resources/js/rocky/service.js
@@ -1,10 +1,4 @@
 (function() {
-	celloudApp.service("uploadService", function($resource) {
-		this.getProductTags = function(){
-			return $resource("uploadFile/getProductTag");
-		}
-	});
-	
 	celloudApp.service("rockyReportService", function($http){
 		this.pageQuery = function(params){
 			return $http({method:"POST",url:'report/rocky/reportMain',data:$.param({"batches":params.batches,"periods":params.periods,"page":params.page,"size":params.size,"beginDate":params.beginDate,"endDate":params.endDate,"sample":params.sample,"sidx":params.sidx,"sord":params.sord}),headers: { 'Content-Type': 'application/x-www-form-urlencoded' }});
