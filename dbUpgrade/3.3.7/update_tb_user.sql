8f4265650d11ac746281fd8c3f2b37db1e3664f4
miaoqi <2127983862@qq.com>
2016-11-03 18:29:23 +0800

    update:更新sql语句

---
+++ dbUpgrade/3.3.7/update_tb_user.sql
@@ -0,0 +1,5 @@
+/*!40101 SET NAMES utf8 */;
+
+alter table tb_user add column province varchar(50);
+alter table tb_user add column city varchar(50);
+alter table tb_user add column district varchar(50);
