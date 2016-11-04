c500e7c949a6f2e4eccb09c7f759b03d152ddbb0
daisly <leamoqinger_2011@163.com>
2016-11-04 09:53:43 +0800

    update:C端用户登录成功跳转loading

--- celloud/src/main/java/com/celloud/action/LoginAction.java
+++ celloud/src/main/java/com/celloud/action/LoginAction.java
@@ -128,7 +128,7 @@ public class LoginAction {
 					logger.info("用户登录失败，未知异常：phone={},captcha={}", cellphone, captcha);
 				}
 				if (subject.isAuthenticated()) {
-					mv.setViewName("client/index");
+                    mv.setViewName("loading");
 					try {
 						FileUtils.forceDelete(new File(PropertiesUtil.outputPath + cellphone));
 					} catch (IOException e) {
