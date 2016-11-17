2fa4857c74ca0dc959831c4749b7ca575491555e
window sun <sun8wd@gmail.com>
2016-11-17 18:29:14 +0800

    update:修改盒子应用的启动脚本，支持动态传参

--- box-api/script/box-service.sh
+++ box-api/script/box-service.sh
@@ -16,8 +16,10 @@ function start(){
 		echo 'Application is already running !'
 	else
         opts=""
+        echo "Start application with args : "
         for opt in ${opt_array[@]}
         do
+            echo "    $opt"
             opts="$opts $opt"
         done
         nohup java -jar $jarfile $opts > $filepath/nohup.out 2>&1 &
@@ -80,7 +82,8 @@ function check(){
 		fi
 	fi
 }
-
+args=($@)
+opt_array=(${opt_array[*]} ${args[*]:1})
 case "$1" in 
 	start)
 		start
