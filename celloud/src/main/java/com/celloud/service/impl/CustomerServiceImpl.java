d0d110c506ba27561556dbec64ec0e14d55c0423
miaoqi <2127983862@qq.com>
2016-12-06 14:28:03 +0800

    update:更新平台验证码发送功能

--- celloud/src/main/java/com/celloud/service/impl/CustomerServiceImpl.java
+++ celloud/src/main/java/com/celloud/service/impl/CustomerServiceImpl.java
@@ -22,29 +22,34 @@ public class CustomerServiceImpl implements CustomerService {
 
     @Override
     public Boolean addOrUpdateUserCaptcha(String cellphone, String captcha) {
-        // 1. 根据cellphone查询mongo中是否包含该手机
-        Map<String, Object> queryFilters = new HashMap<>();
-        queryFilters.put("cellphone", cellphone);
-        List<UserCaptcha> list = reportDao.queryByFilters(UserCaptcha.class, queryFilters, null);
-        UserCaptcha userCaptcha = null;
-        if (list != null && list.size() > 0) {
-            userCaptcha = list.get(0);
-        }
-        if (userCaptcha != null) { // 2. 有就更新
-            Map<String, Object> updateFilters = new HashMap<String, Object>();
-            updateFilters.put("captcha", captcha);
-            updateFilters.put("createDate", new Date());
-            reportDao.update(UserCaptcha.class, queryFilters, updateFilters).intValue();
-        } else { // 3. 没有就插入
-            userCaptcha = new UserCaptcha();
-            userCaptcha.setCaptcha(captcha);
-            // 加上8小时
-            userCaptcha.setCreateDate(new Date());
-            userCaptcha.setCellphone(cellphone);
-            reportDao.saveObj(userCaptcha);
-        }
+
+        // 1. 发送验证码
         String result = AliDayuUtils.sendCaptcha(cellphone, captcha);
-        return !StringUtils.equals(result, "error");
+        Boolean flag = !StringUtils.equals(result, "error");
+        if (flag) { // 发送成功
+            // 2. 根据cellphone查询mongo中是否包含该手机
+            Map<String, Object> queryFilters = new HashMap<>();
+            queryFilters.put("cellphone", cellphone);
+            List<UserCaptcha> list = reportDao.queryByFilters(UserCaptcha.class, queryFilters, null);
+            UserCaptcha userCaptcha = null;
+            if (list != null && list.size() > 0) {
+                userCaptcha = list.get(0);
+            }
+            if (userCaptcha != null) { // 2. 有就更新
+                Map<String, Object> updateFilters = new HashMap<String, Object>();
+                updateFilters.put("captcha", captcha);
+                updateFilters.put("createDate", new Date());
+                reportDao.update(UserCaptcha.class, queryFilters, updateFilters).intValue();
+            } else { // 3. 没有就插入
+                userCaptcha = new UserCaptcha();
+                userCaptcha.setCaptcha(captcha);
+                // 加上8小时
+                userCaptcha.setCreateDate(new Date());
+                userCaptcha.setCellphone(cellphone);
+                reportDao.saveObj(userCaptcha);
+            }
+        }
+        return flag;
     }
 
     @Override
