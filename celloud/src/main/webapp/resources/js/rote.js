434c0ddb221b3841171d2aef938884fbf7422556
huizi666 <huizi0716@163.com>
2016-12-27 14:09:05 +0800

    update:花木兰微信报告

--- celloud/src/main/webapp/resources/js/rote.js
+++ celloud/src/main/webapp/resources/js/rote.js
@@ -59,6 +59,7 @@ $(document).ready(function() {
 		 port=':8080';
 	 }
 	 var webService=protocol+"//"+hostname+port+"/celloud/api/report/getRockyReport?projectId="+request('projectId')+"&dataKey="+request("dataKey")+"&appId="+request('appId');
+	 var webService='https://www.celloud.cc/wechat_rocky.html?projectId=18560&dataKey=16122605441381&appId=123';
 	$.ajax({
 		type: "get",
 		url: webService,
@@ -155,7 +156,7 @@ $(document).ready(function() {
 					tab2 = '<tr class="p2_tab_body">' +
 						'<td>' + (parseInt(i) + 1) + '</td>' +
 						'<td><span id="BCRA">' + record[i].gene + '</span>:<br> ' + record[i].acids + '<br>临床意义:<br>' + record[i].significance + '</td>' +
-						'<td>' + record.description + '</td>' +
+						'<td>' + record[i].description + '</td>' +
 						'</tr>';
 					p1 = '<div class="report_title">' +
 						'<img src="'+window.CONTEXT_PATH+'/images/wechat/rocky_report/logo 3.png"/>' +
