a5fbb2b3c6c57d5f7ea2c8295dfa9872936a3d0d
miaoqi <2127983862@qq.com>
2016-12-29 17:49:12 +0800

    add:增加索引sql

---
+++ dbUpgrade/3.4.2/add_index.sql
@@ -0,0 +1,5 @@
+/*!40101 SET NAMES utf8 */;
+
+ALTER TABLE tb_notice_user ADD INDEX user_id(user_id);
+ALTER TABLE tb_notice_user ADD INDEX state(state);
+ALTER TABLE tb_notice ADD INDEX type(type);
