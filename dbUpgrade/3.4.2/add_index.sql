2da71696cddc2592a67c080822d278f730ddd051
miaoqi <2127983862@qq.com>
2016-12-30 13:45:19 +0800

    update:更新索引sql

--- dbUpgrade/3.4.2/add_index.sql
+++ dbUpgrade/3.4.2/add_index.sql
@@ -3,3 +3,4 @@
 ALTER TABLE tb_notice_user ADD INDEX user_id(user_id);
 ALTER TABLE tb_notice_user ADD INDEX state(state);
 ALTER TABLE tb_notice ADD INDEX type(type);
+ALTER TABLE tb_notice ADD INDEX create_date(create_date);
