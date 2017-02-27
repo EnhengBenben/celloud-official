9c493f84778969dec1503067260fc67c6008eb16
linyongchao <lycwork@126.com>
2017-02-27 17:51:36 +0800

    update:百菌探数据上传到oss

--- celloud/src/main/java/com/celloud/service/impl/DataServiceImpl.java
+++ celloud/src/main/java/com/celloud/service/impl/DataServiceImpl.java
@@ -413,7 +413,8 @@ public class DataServiceImpl implements DataService {
 		data.setFileId(dataId);
 		data.setDataKey(fileDataKey);
 		data.setSize(size);
-		data.setBatch(metadata.get("batch"));
+		String batch = metadata.get("batch");
+		data.setBatch(batch);
 		data.setMd5(metadata.get("etag").toLowerCase());
 		data.setFileName(name);
 		data.setState(DataState.ACTIVE);
@@ -441,8 +442,11 @@ public class DataServiceImpl implements DataService {
 		dataFileMapper.updateByPrimaryKeySelective(data);
 
 		data = dataFileMapper.selectByPrimaryKey(dataId);
-		// TODO 需要根据tagId判断是否rocky
-		runService.rockyCheckRun(123, data);
+		if (tagId == 1) {
+			runService.bsiCheckRun(batch, dataId, fileDataKey, name, userId, fileFormat);
+		} else if (tagId == 2) {
+			runService.rockyCheckRun(123, data);
+		}
 		return dataId;
 	}
 
