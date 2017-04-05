4287742f5c29322b04e549a4745fee5c230763fa
linyongchao <lycwork@126.com>
2017-04-05 17:28:31 +0800

    update:修改了AccuSeqα2、CMP_199、MIB、GDD的运行命令

---
+++ dbUpgrade/3.4.12/app_command.sql
@@ -0,0 +1,36 @@
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 4096
+#
+# http://www.sequelpro.com/
+# http://code.google.com/p/sequel-pro/
+#
+# Host: 127.0.0.1 (MySQL 10.1.8-MariaDB)
+# Database: celloud
+# Generation Time: 2017-03-08 06:26:34 +0000
+# ************************************************************
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table tb_district
+# ------------------------------------------------------------
+
+update tb_app set command = 'nohup perl /share/biosoft/Script/Script_share/moniter_qsub_url-v1.pl nohup perl /share/biosoft/Script/AccuSeq_2/qsub_50gene_2samples.pl ${list} ${path} ProjectID${projectId} &>${path}${projectId}.log' where app_id = 131;
+update tb_app set command = 'nohup perl /perl /share/biosoft/Script/Script_share/moniter_qsub_url-v1.pl nohup perl /share/biosoft/Script/CMP_199/qsub_199gene.pl ${list} ${path} ProjectID${projectId} &>${path}${projectId}.log' where app_id = 111;
+update tb_app set command = 'nohup perl perl /share/biosoft/Script/Script_share/moniter_qsub_url-v1.pl nohup perl /share/biosoft/Script/GDD/qsub_GDD.pl ${list} ${path} ProjectID${projectId} &>${path}${projectId}.log' where app_id = 112;
+update tb_app set command = 'nohup perl perl /share/biosoft/Script/Script_share/moniter_qsub_url-v1.pl nohup perl /share/biosoft/Script/MIB/qsub_16sMIB.pl ${list} ${path} ProjectID${projectId} &>${path}${projectId}.log' where app_id = 114;
+update tb_app set command = 'nohup perl /share/biosoft/Script/Script_share/moniter_qsub_url-v1.pl nohup perl /share/biosoft/Script/AccuSeq_2/qsub_50gene_2samples.pl ${list} ${path} ProjectID${projectId} &>${path}${projectId}.log' where app_id = 131;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
