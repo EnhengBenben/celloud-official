ed29ca0e7bd5eec7eb25a983e95674375db68837
EnhengBenben <wangyong813111@163.com>
2017-04-17 16:33:24 +0800

    删除无用代码

--- gulp/scripts.js
+++ gulp/scripts.js
@@ -1,22 +1,4 @@
-/*
-'use strict';
 
-var path = require('path');
-var gulp = require('gulp');
-var conf = require('./conf');
-
-var browserSync = require('browser-sync');
-
-var $ = require('gulp-load-plugins')();
-
-gulp.task('scripts', function () {
-  return gulp.src(path.join(conf.paths.src, '/app/!**!/!*.js'))
-    .pipe($.jshint())
-    .pipe($.jshint.reporter('jshint-stylish'))
-    .pipe(browserSync.reload({ stream: true }))
-    .pipe($.size())
-});
-*/
 'use strict';
 
 var path = require('path');
